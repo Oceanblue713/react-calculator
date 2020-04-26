d9de3e654cba50d2c5857085ef3b4f221ca26278
Anri Liy <amedamachip713@gmail.com>
2020-04-26 15:10:55 -0700

    Add CSS and OZ

--- src/box.js
+++ src/box.js
@@ -5,7 +5,7 @@ const Box = (props) => {
     <div className="box">
       <h3>{props.bMetric} to {props.aMetric}</h3>
       <div className="box-item">
-        <input type="text" value={props.bCalc} onChange={props.bCalcChange}/>{props.bMetric} <br/>
+        <input type="text" value={props.bCalc} onChange={props.bCalcChange}/> {props.bMetric} <br/>
         <h1>=</h1>
   <h1>{props.aCalc} {props.aMetric}</h1>
       </div>
