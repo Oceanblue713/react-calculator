7b84db2cbfd3021c1cefb789fcdd1e44d2e5583f
Anri Liy <amedamachip713@gmail.com>
2020-05-06 13:53:42 -0700

    Complete CSS

--- src/App.js
+++ src/App.js
@@ -2,60 +2,16 @@ import React from 'react';
 import Box from './box';
 import './App.css';
 import {Nav} from './Nav';
+import Calculator from './Calculator';
 import Time from './Time';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
-
-  handleKgChange(event){
-    this.setState({aCalc1: (event.target.value * 0.454).toFixed(2)});
-  }
-
-  handleCmChange(event){
-    this.setState({aCalc2: (event.target.value * 30.48).toFixed(2)});
-  }
-
-  handleMlChange(event){
-    this.setState({aCalc3: (event.target.value * 29.57).toFixed(2)});
-  }
-
-  handleKmChange(event){
-    this.setState({aCalc4: (event.target.value * 1.609).toFixed(3)});
-  }
-
   render(){
-    const bCalcState = this.state.value
-
     return (
       <div className="App">
         <Nav />
         <Time />
-        <Box bCalc={bCalcState} 
-             bCalcChange={(event) => this.handleKgChange(event)}
-             bMetric="lb" 
-             aCalc = {this.state.aCalc1} 
-             aMetric="Kg" />
-
-        <Box bCalc={bCalcState}
-             bCalcChange={(event) => this.handleCmChange(event)}
-             bMetric="feet"
-             aCalc={this.state.aCalc2}
-             aMetric="cm" />
-
-        <Box bCalc={bCalcState}
-             bCalcChange={(event) => this.handleMlChange(event)}
-             bMetric="oz"
-             aCalc={this.state.aCalc3}
-             aMetric="ml" />
-
-        <Box bCalc={bCalcState} 
-             bCalcChange={(event) => this.handleKmChange(event)}
-             bMetric="mile"
-             aCalc={this.state.aCalc4}
-             aMetric="Km" />
+        <Calculator />
       </div>
     );
   }
---
+++ src/Calculator.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import Box from './box';
+
+class Calculator extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  handleKgChange(event){
+    this.setState({aCalc1: (event.target.value * 0.454).toFixed(2)});
+  }
+
+  handleCmChange(event){
+    this.setState({aCalc2: (event.target.value * 30.48).toFixed(2)});
+  }
+
+  handleMlChange(event){
+    this.setState({aCalc3: (event.target.value * 29.57).toFixed(2)});
+  }
+
+  handleKmChange(event){
+    this.setState({aCalc4: (event.target.value * 1.609).toFixed(3)});
+  }
+
+  handleInchCmChange(event){
+    this.setState({aCalc5: (event.target.value * 2.54).toFixed(2)});
+  }
+
+  handleCelsiusFahChange(event) {
+    this.setState({aCalc6: (event.target.value * 9/5 + 32).toFixed(2)});
+  }
+
+  render(){
+    const bCalcState = this.state.value
+
+    return (
+      <div className="flexbox-container">
+        <Box bCalc={bCalcState}
+             bCalcChange={(event) => this.handleMlChange(event)}
+             bMetric="oz"
+             aCalc={this.state.aCalc3}
+             aMetric="ml" />
+
+        <Box bCalc={bCalcState} 
+             bCalcChange={(event) => this.handleKgChange(event)}
+             bMetric="lb" 
+             aCalc = {this.state.aCalc1} 
+             aMetric="Kg" />
+
+        <Box bCalc={bCalcState}
+             bCalcChange={(event) => this.handleCelsiusFahChange(event)}
+             bMetric="&#8451;"
+             aCalc={this.state.aCalc6}
+             aMetric="F" />
+
+        <Box bCalc={bCalcState}
+             bCalcChange={(event) => this.handleInchCmChange(event)}
+             bMetric="Inch"
+             aCalc={this.state.aCalc5}
+             aMetric="cm" />
+
+        <Box bCalc={bCalcState}
+             bCalcChange={(event) => this.handleCmChange(event)}
+             bMetric="feet"
+             aCalc={this.state.aCalc2}
+             aMetric="cm" />
+
+        <Box bCalc={bCalcState} 
+             bCalcChange={(event) => this.handleKmChange(event)}
+             bMetric="mile"
+             aCalc={this.state.aCalc4}
+             aMetric="Km" />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
